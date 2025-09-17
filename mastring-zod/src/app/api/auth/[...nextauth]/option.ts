af345473244060d339106a0379eaa98168d4aa1c
vipinrao009 <vipinrao2018@gmail.com>
2025-09-17 14:19:24 +0530

    feat: implement authentication with NextAuth and user model integration

---
+++ mastring-zod/src/app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,75 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier.email },
+              { username: credentials.identifier.username },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify the email before login");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.identifier.password,
+            user.password
+          ); // yha par password directly mil jate hai hame identifire use kaene ki need nahi padti hai
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Please enter the correct password");
+          }
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString(); // yaha par directly _id accept nahi karega hame types me jaker define karana padega
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
