df93d804d77868d959e15791e07b84b2593a1188
vipinrao009 <vipinrao2018@gmail.com>
2025-09-19 17:11:14 +0530

    Added account verification

---
+++ mastring-zod/src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { Loader } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import * as z from "zod";
+
+const VerifyAccount = () => {
+  const [isVerifying, setIsVerifying] = useState(false)
+  const params = useParams();
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setIsVerifying(true)
+    try {
+      const response = await axios.post(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+
+      if (response) {
+        toast.success(response?.data.message);
+      }
+      router.replace("/sign-in");
+      setIsVerifying(false)
+    } catch (error) {
+      console.log("Error in signup of user");
+      const AxiosError = error as AxiosError<ApiResponse>;
+      let Error = AxiosError.response?.data.message;
+      toast.error(Error);
+    }finally{
+        setIsVerifying(false)
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-4xl font-extrabold tracking-tight mb-6">
+          Verify Your Account
+        </h1>
+        <p>Enter the verification code sent to your email</p>
+
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Verification Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="code" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-center">
+                <Button disabled={isVerifying} type="submit">{ isVerifying ? (
+                    <>
+                    <Loader/> Please wait
+                    </>
+                ) : ("Verify Account")}</Button>
+              </div> 
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyAccount;
